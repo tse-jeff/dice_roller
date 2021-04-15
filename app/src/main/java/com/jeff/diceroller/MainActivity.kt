01ee91d2a1fc8a0476cce072fe44e3c1eeaa63a3
tse-jeff <jtse0316@gmail.com>
2021-04-15 02:54:33 -0400

    toast message from button click

--- app/src/main/java/com/jeff/diceroller/MainActivity.kt
+++ app/src/main/java/com/jeff/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jeff.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
