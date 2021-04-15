d8784d23e11672cdffa322e086278b34e41b9189
tse-jeff <jtse0316@gmail.com>
2021-04-15 03:12:04 -0400

    functioning dice rolling app: bare

--- app/src/main/java/com/jeff/diceroller/MainActivity.kt
+++ app/src/main/java/com/jeff/diceroller/MainActivity.kt
@@ -3,16 +3,43 @@ package com.jeff.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+//import android.widget.Toast
 
+/**
+ * This activity allows the user to roll a die and view the result
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
-        }
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * The rollDice function creates a Dice object of 6 sides and adds
+     * the result to the textView.
+     */
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+
+//        This follow line allows for a pop up on the bottom of the screen
+//        that shows a text.
+//        Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+    }
+}
+
+/**
+ * The Dice class has sides and a function to roll and return a random
+ * number within the range.
+ */
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
