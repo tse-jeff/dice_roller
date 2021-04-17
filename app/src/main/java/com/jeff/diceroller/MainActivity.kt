868c03366c156e43df2f6b44d26b0920905d3906
tse-jeff <jtse0316@gmail.com>
2021-04-16 23:32:22 -0400

    added a second die

--- app/src/main/java/com/jeff/diceroller/MainActivity.kt
+++ app/src/main/java/com/jeff/diceroller/MainActivity.kt
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 
 //import android.widget.Toast
 
@@ -30,15 +28,36 @@ class MainActivity : AppCompatActivity() {
      * the result to the textView.
      */
     private fun rollDice() {
-        // create dice object with 6 sides
-        val dice = Dice(6)
-        val roll = dice.roll()
+        // 2 dice
+        val numDice = 2
+
+        // create dice object with 6 sides (experimenting with Kotlin arrays)
+        val dice = Array(numDice) { Dice(6) }
+
+        // roll both dice (using a for loop as practice)
+        val rolls = IntArray(numDice)
+        for (i in dice.indices) {
+            rolls[i] = dice[i].roll()
+        }
 
         // find the ImageView in the layout
-        val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage1: ImageView = findViewById(R.id.die1)
+        val diceImage2: ImageView = findViewById(R.id.die2)
 
-        // determine which drawable to show based on the roll
-        val drawableResource = when (roll) {
+        /*
+        determine which drawable to show based on the roll
+        haven't really figured out how to make these two iterations with a loop
+        because I haven't fully grasped the implementation of Arrays and Lists in Kotlin
+         */
+        val pic1 = when (rolls[0]) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val pic2 = when (rolls[1]) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -48,10 +67,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         // update the ImageView with the correct drawable resource ID
-        diceImage.setImageResource(drawableResource)
+        diceImage1.setImageResource(pic1)
+        diceImage2.setImageResource(pic2)
 
         // update the content description
-        diceImage.contentDescription = roll.toString()
+        diceImage1.contentDescription = rolls[0].toString()
+        diceImage2.contentDescription = rolls[1].toString()
 
 //        This follow line allows for a pop up on the bottom of the screen
 //        that shows a text.
