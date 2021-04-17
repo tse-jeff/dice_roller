b80b12e57114dde57e039323d5dc976c103e1ca4
tse-jeff <jtse0316@gmail.com>
2021-04-16 22:47:23 -0400

    images for the dice

--- app/src/main/java/com/jeff/diceroller/MainActivity.kt
+++ app/src/main/java/com/jeff/diceroller/MainActivity.kt
@@ -3,7 +3,10 @@ package com.jeff.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+
 //import android.widget.Toast
 
 /**
@@ -14,11 +17,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // adds an event listener for a roll button
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-            findViewById<TextView>(R.id.textView2).text = "Click Roll to re-roll"
-        }
+        rollButton.setOnClickListener { rollDice() }
+
+        // random roll when app starts
+        rollDice()
     }
 
     /**
@@ -26,10 +30,28 @@ class MainActivity : AppCompatActivity() {
      * the result to the textView.
      */
     private fun rollDice() {
+        // create dice object with 6 sides
         val dice = Dice(6)
-        val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.number)
-        resultTextView.text = diceRoll.toString()
+        val roll = dice.roll()
+
+        // find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // determine which drawable to show based on the roll
+        val drawableResource = when (roll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // update the content description
+        diceImage.contentDescription = roll.toString()
 
 //        This follow line allows for a pop up on the bottom of the screen
 //        that shows a text.
